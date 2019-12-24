0e275e856d06e18ccba16ad0a72ca97fcd31a994
Hansiyuan131 <422295068@qq.com>
2019-12-24 13:52:12 +0800

    添加scrapy爬虫

---
+++ README.md
@@ -0,0 +1,5 @@
+# 爬虫
+
+## urllib爬虫项目
+## request爬虫项目
+## scrapy爬虫项目
---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/24 9:40
+# @Author  : SiYuan Han
+# @Email   : 422295068@qq.com
+# @File    : test.py
+# @Software: PyCharm
+
+import django
+
+print(django.get_version())
